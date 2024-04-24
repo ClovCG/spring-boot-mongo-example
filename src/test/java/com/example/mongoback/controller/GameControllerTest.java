ed57bdec0717a5d85fc568c28563dab8d901b8bf
César García <cesarg92e@gmail.com>
2024-04-24 14:17:51 +0200

    Added test classes.

---
+++ src/test/java/com/example/mongoback/controller/GameControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.mongoback.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.mongoback.service.GameService;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(GameController.class)
+public class GameControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private GameService gameService;
+
+}
---
+++ src/test/java/com/example/mongoback/service/GameServiceTest.java
@@ -0,0 +1,19 @@
+package com.example.mongoback.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.mongoback.repository.GameRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class GameServiceTest {
+
+    @Mock
+    private GameRepository gameRepository;
+
+    @InjectMocks
+    private GameService gameService;
+
+}
