7857b0acb39ec08688d48461f54f962aeff1ae3f
César García <cesarg92e@gmail.com>
2024-05-01 14:03:36 +0200

    Added more test cases.

--- src/test/java/com/example/mongoback/mapper/GameMapperTest.java
+++ src/test/java/com/example/mongoback/mapper/GameMapperTest.java
@@ -1,7 +1,9 @@
 package com.example.mongoback.mapper;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.List;
 
@@ -54,18 +56,23 @@ public class GameMapperTest {
     }
 
     @Test
-    public void givenGameWithEmptyProperties_whenMapToGameDTO_thenEquals() {
+    public void givenGameWithNullProperties_whenMapToGameDTO_thenEquals() {
         // Given
         Game game = new Game();
 
         // When
         GameDTO gameDto = mapper.map(game, GameDTO.class);
 
-        // Then
+        // Then (assert null properties, then assert equals)
+        assertNull(game.getTitle());
         assertEquals(game.getTitle(), gameDto.getTitle());
+        assertNull(game.getDeveloper());
         assertEquals(game.getDeveloper(), gameDto.getDeveloper());
+        assertNull(game.getPublisher());
         assertEquals(game.getPublisher(), gameDto.getPublisher());
+        assertNull(game.getYear());
         assertEquals(game.getYear(), gameDto.getYear());
+        assertNull(game.getPlatforms());
         assertIterableEquals(game.getPlatforms(), gameDto.getPlatforms());
     }
 
@@ -77,11 +84,58 @@ public class GameMapperTest {
         // When
         Game game = mapper.map(gameDto, Game.class);
 
-        // Then
+        // Then (assert null properties, then assert equals)
+        assertNull(gameDto.getTitle());
         assertEquals(gameDto.getTitle(), game.getTitle());
+        assertNull(gameDto.getDeveloper());
         assertEquals(gameDto.getDeveloper(), game.getDeveloper());
+        assertNull(gameDto.getPublisher());
         assertEquals(gameDto.getPublisher(), game.getPublisher());
+        assertNull(gameDto.getYear());
         assertEquals(gameDto.getYear(), game.getYear());
+        assertNull(gameDto.getPlatforms());
+        assertIterableEquals(gameDto.getPlatforms(), game.getPlatforms());
+    }
+
+    @Test
+    public void givenGameWithEmptyProperties_whenMapToGameDTO_thenEquals() {
+        // Given
+        Game game = new Game("", "", "", "", List.of(""));
+
+        // When
+        GameDTO gameDto = mapper.map(game, GameDTO.class);
+
+        // Then (assert empty strings, then assert equals)
+        assertEquals(game.getTitle(), "");
+        assertEquals(game.getTitle(), gameDto.getTitle());
+        assertEquals(game.getDeveloper(), "");
+        assertEquals(game.getDeveloper(), gameDto.getDeveloper());
+        assertEquals(game.getPublisher(), "");
+        assertEquals(game.getPublisher(), gameDto.getPublisher());
+        assertEquals(game.getYear(), "");
+        assertEquals(game.getYear(), gameDto.getYear());
+        assertArrayEquals(game.getPlatforms().toArray(), List.of("").toArray());
+        assertIterableEquals(game.getPlatforms(), gameDto.getPlatforms());
+    }
+
+    @Test
+    public void givenGameDTOWithEmptyProperties_whenMapToGame_thenEquals() {
+        // Given
+        GameDTO gameDto = new GameDTO("", "", "", "", List.of(""));
+
+        // When
+        Game game = mapper.map(gameDto, Game.class);
+
+        // Then (assert empty strings, then assert equals)
+        assertEquals(gameDto.getTitle(), "");
+        assertEquals(gameDto.getTitle(), game.getTitle());
+        assertEquals(gameDto.getDeveloper(), "");
+        assertEquals(gameDto.getDeveloper(), game.getDeveloper());
+        assertEquals(gameDto.getPublisher(), "");
+        assertEquals(gameDto.getPublisher(), game.getPublisher());
+        assertEquals(gameDto.getYear(), "");
+        assertEquals(gameDto.getYear(), game.getYear());
+        assertArrayEquals(gameDto.getPlatforms().toArray(), List.of("").toArray());
         assertIterableEquals(gameDto.getPlatforms(), game.getPlatforms());
     }
 
--- src/test/java/com/example/mongoback/service/GameServiceTest.java
+++ src/test/java/com/example/mongoback/service/GameServiceTest.java
@@ -1,19 +1,61 @@
 package com.example.mongoback.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 
+import com.example.mongoback.dto.GameDTO;
+import com.example.mongoback.model.Game;
 import com.example.mongoback.repository.GameRepository;
 
 @ExtendWith(MockitoExtension.class)
 public class GameServiceTest {
 
+    @InjectMocks
+    private GameServiceImpl gameService;
+
     @Mock
     private GameRepository gameRepository;
 
-    @InjectMocks
-    private GameServiceImpl gameService;
+    @Mock
+    private ModelMapper mapper;
+
+    @BeforeEach
+    public void beforeEach() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void givenGameDTO_whenSaveGame_thenReturnGame() {
+        // Given
+        GameDTO gameDetails = new GameDTO("Tetrimino", "Tetrimino Guy", "Tots", "1984", List.of("Puzzle"));
+        Game game = new Game("Tetrimino", "Tetrimino Guy", "Tots", "1984", List.of("Puzzle"));
+        Game savedGame = new Game("Tetrimino", "Tetrimino Guy", "Tots", "1984", List.of("Puzzle"));
+
+        // Mocks
+        when(mapper.map(gameDetails, Game.class)).thenReturn(game);
+        when(gameRepository.save(game)).thenReturn(savedGame);
+
+        // When
+        Game responseGame = gameService.saveGame(gameDetails);
+
+        // Then
+        assertEquals(responseGame.getTitle(), savedGame.getTitle());
+        assertEquals(responseGame.getDeveloper(), savedGame.getDeveloper());
+        assertEquals(responseGame.getPublisher(), savedGame.getPublisher());
+        assertEquals(responseGame.getYear(), savedGame.getYear());
+        assertIterableEquals(responseGame.getPlatforms(), savedGame.getPlatforms());
+    }
 
 }
